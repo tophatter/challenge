767888463a7f4b439d41817511e92ed0a9012f2d
Leo Qin <leoq@tophatter.com>
2020-01-21 09:47:12 -0800

    Print every loop, simplify to assume we only update every percent

--- python/run_solver.py
+++ python/run_solver.py
@@ -6,16 +6,13 @@ from board import Board
 def run_solver(n, num_mines, num_trials):
     wins = 0
     start_time = time.time()
-    INTERVAL = 1
-    current_percentage = 0
     for i in range(num_trials):
         board = Board(n, num_mines)
         if solve(board):
             wins += 1
 
-        if (i+1) * 100 / num_trials >= current_percentage + INTERVAL:
-            current_percentage += INTERVAL
-            print(f'\r{current_percentage}% complete ({i+1}/{num_trials})', end='')
+        current_percentage = (i + 1) * 100 // num_trials
+        print(f'\r{current_percentage}% complete ({i+1}/{num_trials})', end='')
 
     finish_time = time.time()
     print()
